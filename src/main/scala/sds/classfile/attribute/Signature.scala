dc2d0a1638268804bae4c6bfeceb6f4e1a935048
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-04-11 12:10:35 +0900

    modify sigunature.

--- src/main/scala/sds/classfile/attribute/Signature.scala
+++ src/main/scala/sds/classfile/attribute/Signature.scala
@@ -2,9 +2,10 @@ package sds.classfile.attribute
 
 import sds.classfile.ClassfileStream
 import sds.classfile.constant_pool.ConstantInfo
+import sds.util.DescriptorParser.parse
 
 class Signature(data: ClassfileStream, pool: Array[ConstantInfo]) extends AttributeInfo {
-	private val signature: String = extract(data.readShort(), pool)
+	private val signature: String = parse(extract(data.readShort(), pool), true)
 
 	def getSignature(): String = signature
 	override def toString(): String = super.toString() + ": " + signature
