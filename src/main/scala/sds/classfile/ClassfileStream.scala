f39688179f3ecd99b45a0c9a4f327176d4c22549
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-04-12 19:39:50 +0900

    add method for incrementing file pointer.

--- src/main/scala/sds/classfile/ClassfileStream.scala
+++ src/main/scala/sds/classfile/ClassfileStream.scala
@@ -1,7 +1,19 @@
 package sds.classfile
 
-import java.lang.{Byte => By, Character, Double => Do, Float => Fl, Integer, Long => Lo, Short}
-import java.io.{DataInputStream, InputStream, RandomAccessFile}
+import java.lang.{
+  Byte   => By,
+  Double => Do,
+  Float  => Fl,
+  Long   => Lo,
+  Character,
+  Integer,
+  Short
+}
+import java.io.{
+  DataInputStream,
+  InputStream,
+  RandomAccessFile
+}
 
 class ClassfileStream extends AutoCloseable {
 	var stream: DataInputStream = null;
@@ -18,63 +30,27 @@ class ClassfileStream extends AutoCloseable {
 		this.stream = new DataInputStream(stream);
 	}
 
-	def readByte(): Int = {
-		pointer += By.BYTES
-		if(raf != null) raf.readByte() else stream.readByte()
-	}
-
-	def readUnsignedByte(): Int = {
-		pointer += By.BYTES
-		if(raf != null) raf.readUnsignedByte() else stream.readUnsignedByte()
-	}
-
-	def readChar(): Char = {
-		pointer += Character.BYTES
-		if(raf != null) raf.readChar() else stream.readChar()
-	}
-
-	def readDouble(): Double = {
-		pointer += Do.BYTES
-		if(raf != null) raf.readDouble() else stream.readDouble()
-	}
-
-	def readFloat(): Float = {
-		pointer += Fl.BYTES
-		if(raf != null) raf.readFloat() else stream.readFloat()
-	}
-
-	def readInt(): Int = {
-		pointer += Integer.BYTES
-		if(raf != null) raf.readInt() else stream.readInt()
-	}
-
-	def readLong(): Long = {
-		pointer += Lo.BYTES
-		if(raf != null) raf.readLong() else stream.readLong()
-	}
-
-	def readShort(): Int = {
-		pointer += Short.BYTES
-		if(raf != null) raf.readShort() else stream.readShort()
-	}
-
-	def readUnsignedShort(): Int = {
-		pointer += Short.BYTES
-		if(raf != null) raf.readUnsignedShort() else stream.readUnsignedShort()
-	}
-
+	def skipBytes(n: Int):   Unit   = if(isRaf(n))               raf.skipBytes(n) else stream.skipBytes(n)
+	def readByte():          Int    = if(isRaf(By.BYTES))        raf.readByte()   else stream.readByte()
+	def readChar():          Char   = if(isRaf(Character.BYTES)) raf.readChar()   else stream.readChar()
+	def readDouble():        Double = if(isRaf(Do.BYTES))        raf.readDouble() else stream.readDouble()
+	def readFloat():         Float  = if(isRaf(Fl.BYTES))        raf.readFloat()  else stream.readFloat()
+	def readInt():           Int    = if(isRaf(Integer.BYTES))   raf.readInt()    else stream.readInt()
+	def readLong():          Long   = if(isRaf(Lo.BYTES))        raf.readLong()   else stream.readLong()
+	def readShort():         Int    = if(isRaf(Short.BYTES))     raf.readShort()  else stream.readShort()
+	def readUnsignedByte():  Int    = if(isRaf(By.BYTES))    raf.readUnsignedByte()  else stream.readUnsignedByte()
+	def readUnsignedShort(): Int    = if(isRaf(Short.BYTES)) raf.readUnsignedShort() else stream.readUnsignedShort()
+	
 	def readFully(b: Array[Byte]): Array[Byte] = {
-		pointer += b.length
-		if(raf != null) raf.readFully(b) else stream.readFully(b)
+		if(isRaf(b.length)) raf.readFully(b) else stream.readFully(b)
 		b
 	}
 
-	def skipBytes(n: Int): Unit = {
-		pointer += n
-		if(raf != null) raf.skipBytes(n) else stream.skipBytes(n)
+	def isRaf(bytes: Int): Boolean = {
+		pointer += bytes
+		raf != null
 	}
 
 	def getFilePointer(): Long = if(raf != null) raf.getFilePointer() else pointer
-
 	override def close(): Unit = if(raf != null) raf.close() else stream.close()
 }
